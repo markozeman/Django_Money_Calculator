066dd22d05f5356d47a71bd5913a708dfea8b883
markozeman <mz4730@student.uni-lj.si>
2016-12-20 20:41:01 +0100

    Registration and login

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,70 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+from datetime import datetime
+
+
+
+class Stanje(models.Model):
+    tip = models.CharField(max_length=20, default='')
+    stanje = models.FloatField(default=0)
+
+    ### ForeignKey od Uporanika ????
+
+    #izdatki = models.ForeignKey(IzdatekPrejemek)
+    #prejemki = models.ForeignKey(IzdatekPrejemek)
+
+    def __str__(self):
+        return self.tip
+
+
+class IzdatekPrejemek(models.Model):
+    tip = models.CharField(max_length=20, default='')
+    opis = models.CharField(max_length=100, default='')
+    znesek = models.FloatField()
+    kategorija = models.CharField(max_length=30, default='')
+    datum = models.DateTimeField(default=datetime.now)
+    banka_denarnica = models.CharField(max_length=20, default='')
+    stanje = models.ForeignKey(Stanje, on_delete=models.CASCADE, related_name="izdatki_prejemki")
+
+    def __str__(self):
+        return self.tip
+
+
+class Uporabnik(models.Model):
+    uporabnik = models.OneToOneField(User, on_delete=models.CASCADE)
+    bancno_stanje = models.OneToOneField(Stanje, on_delete=models.CASCADE, related_name="uporabnik_bancno_stanje")
+    denarnica_stanje = models.OneToOneField(Stanje, on_delete=models.CASCADE, related_name="uporabnik_denarnica_stanje")
+    #cilji = models.ManyToOneRel("Cilj", on_delete=models.CASCADE)
+    #transakcije = models.ManyToManyField(Transakcija, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.uporabnik
+
+
+class Transakcija(models.Model):
+    opis = models.CharField(max_length=100, default='')
+    znesek = models.FloatField()
+    smer = models.CharField(max_length=20)
+    uporabnik = models.ForeignKey(Uporabnik, on_delete=models.CASCADE, related_name="transakcije")
+
+    def __str__(self):
+        return self.opis
+
+
+class Cilj(models.Model):
+    opis = models.CharField(max_length=150, default='')
+    vrednost = models.FloatField()
+    trenutno_privarcevano = models.FloatField(default=0)
+    od_datuma = models.DateTimeField(default=datetime.now)
+    do_datuma = models.DateTimeField()
+    uporabnik = models.ForeignKey(Uporabnik, on_delete=models.CASCADE, related_name="cilji")
+
+    def __str__(self):
+        return self.opis
+
+
+
+
--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+from .forms import LoginForm
+from .models import *
+
 
 def registration(request):
+    if (request.method == 'POST'):
+        if (request.POST.get("register")):
+            register_user(request)
+
+        elif (request.POST.get("sign_in")):
+            login_user(request)
+
     return render(request, 'registration/registration_home.html')
 
 
@@ -19,3 +35,70 @@ def pregled(request):
 
 def vizualizacija(request):
     return render(request, 'vizualizacija/vizualizacija_home.html')
+
+
+
+
+
+def register_user(request):
+    usr = request.POST['up_ime']
+    passwd_1 = request.POST['geslo']
+    passwd_2 = request.POST['geslo_2']
+    captcha = request.POST.get('captcha', False)
+
+    # check if passwords are the same, check if username is at least 6 characters long and captcha clicked
+    if (passwd_1 == passwd_2 and len(usr) > 5 and captcha is not False):
+        username_taken = False
+        all_users = list(User.objects.all())
+        for user in all_users:
+            if (user.username == usr):
+                username_taken = True
+                break
+
+        if (not username_taken):
+            new_user = User.objects.create_user(usr, None, passwd_1)
+            new_user.save()
+
+            bancno_stanje = Stanje("banka", 0.0)
+            denarnica_stanje = Stanje("denarnica", 0.0)
+            uporabnik = Uporabnik(new_user, bancno_stanje, denarnica_stanje)
+            uporabnik.save()
+
+            # TODO
+            # save Stanje ???
+
+            # TODO
+            # zakaj ne dela uporabnik ???
+
+            print("\nUser registered.\n")
+        else:
+            print("\nThis username is already taken.\n")
+    else:
+        print("\nPasswords are not the same or username is not longer than 5 characters or captcha is not clicked.\n")
+
+
+
+def login_user(request):
+    username = request.POST['up_ime']
+    password = request.POST.get('geslo', False)
+    password_2 = password = request.POST.get('geslo_2', False)
+
+    if (password is False and password_2 is False):
+        print("Password input is blank.")
+    if (password_2 is not False):
+        password = password_2
+
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        print("Logged in.")
+        # Redirect to a success page.
+    else:
+        # Return an 'invalid login' error message.
+        print("Invalid login")
+
+
+
+def logout_user(request):
+  logout(request)
+  return HttpResponseRedirect(reverse('registracija'))

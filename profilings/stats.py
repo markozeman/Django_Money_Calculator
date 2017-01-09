91ea8fccda1bb88f430679e500621040b6894c09
markozeman <mz4730@student.uni-lj.si>
2017-01-09 23:35:34 +0100

    URL routes diagram added

--- profilings/stats.py
+++ profilings/stats.py
@@ -1,12 +1,11 @@
 import pstats
 
-paths = ["manage.prof", "registration/reg_views.prof", "vnos/vnos_views.prof", "pregled/pregled_views.prof", "vizualizacija/vizualizacija_views.prof"]
+paths = ["profilings/manage.prof", "profilings/reg_views.prof", "profilings/vnos_views.prof", "profilings/pregled_views.prof", "profilings/vizualizacija_views.prof"]
 
 def print_stats(paths):
     for path in paths:
         p = pstats.Stats(path)
         p.sort_stats('cumulative').print_stats(10)
         print("\n\n\n")
-
 
 print_stats(paths)
